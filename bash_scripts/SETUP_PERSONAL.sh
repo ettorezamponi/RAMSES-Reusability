f150f6b12104fbbf9fc254b2df484d540fb13d1b
Ettore Zamponi <ettore.zamponi@mail.polimi.it>
2023-12-23 17:46:05 +0100

    0.6.0 - add json config for teastore

--- bash_scripts/SETUP_PERSONAL.sh
+++ bash_scripts/SETUP_PERSONAL.sh
@@ -8,22 +8,28 @@ export GITHUB_REPOSITORY_URL=https://github.com/ettorezamponi/config-server.git
 export GITHUB_OAUTH=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
 
 if [[("${GITHUB_OAUTH}" = "") || ("${GITHUB_REPOSITORY_URL}" = "")]]; then
+  PrintWarn "Desired architecture not specified or unknown. Supported values are 'arm64' and 'amd64'. Using 'arm64' as default option"
   PrintError "Env var GITHUB_OAUTH and GITHUB_REPOSITORY_URL must be set!"
   exit 1
 fi
 
-PrintWarn "Desired architecture not specified or unknown. Supported values are 'arm64' and 'amd64'. Using 'arm64' as default option"
 ARCH="arm64"
 PrintWarn "Running script with selected architecture: ${ARCH}"
-sleep 3
-
+sleep 1
 
 ##### Network #####
-docker network rm teastore
+docker network rm ramses-sas-net
 PrintSuccess "-- Network Removed --"
-PrintSuccess "Creating new Docker network called 'teastore'"
-docker network create teastore
+PrintSuccess "Creating new Docker network called 'ramses-sas-net'"
+docker network create ramses-sas-net
 echo
+
+##### MYSQL #####
+PrintSuccess "Setting up MySQL Server"
+docker pull giamburrasca/mysql:$ARCH
+docker run -P --name mysql -d --network ramses-sas-net giamburrasca/mysql:$ARCH
+echo
+sleep 2
 
 ##### SEFA #####
 echo; PrintSuccess "Setting up the Managed System, SEFA!"; echo
@@ -38,7 +44,7 @@ PrintSuccess "Setting up Spring Config Server"
 docker pull giamburrasca/sefa-configserver:$ARCH
 docker run -P --name sefa-configserver -e GITHUB_REPOSITORY_URL=$GITHUB_REPOSITORY_URL -d --network ramses-sas-net giamburrasca/sefa-configserver:$ARCH
 echo
-sleep 8
+sleep 15
 
 declare -a arr=("sefa-restaurant-service"
                 "sefa-ordering-service"
@@ -89,13 +95,13 @@ echo
 
 ##### RAMSES #####
 echo; PrintSuccess "Setting up the Managing System, RAMSES!"; echo
-sleep 20
+sleep 10
 
 docker pull giamburrasca/ramses-knowledge:$ARCH
 docker run -P --name ramses-knowledge -d --network ramses-sas-net giamburrasca/ramses-knowledge:$ARCH
 sleep 10
 
-declare -a ramsesarr=("ramses-analyse" "ramses-plan" "ramses-execute" "ramses-monitor" "ramses-dashboard")
+declare -a ramsesarr=("ramses-analyse" "ramses-execute" "ramses-monitor" "ramses-dashboard")
 for i in "${ramsesarr[@]}"
 do
    PrintSuccess "Pulling $i"
@@ -105,6 +111,10 @@ do
    sleep 2
 done
 
+PrintSuccess "Pulling ramses-plan"
+sleep 2
+docker pull giamburrasca/ramses-plan:arm64
+docker run -P --name ramses-plan -d --network ramses-sas-net giamburrasca/ramses-plan:arm64
 
 echo; PrintSuccess "DONE!"; echo
 
