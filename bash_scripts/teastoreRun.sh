626c3ebad1e07cd7b09663e31a7b04d28ac58d07
Ettore Zamponi <ettore.zamponi@mail.polimi.it>
2024-01-30 17:17:18 +0100

    0.9.7.4 - MetricsExporter javamelody + micrometer

--- bash_scripts/teastoreRun.sh
+++ bash_scripts/teastoreRun.sh
@@ -10,40 +10,31 @@ PrintSuccess "Creating new Docker network called 'teastore'"
 docker network create teastore
 echo
 
-PrintSuccess "Setting up TeaStore Registry"
-docker pull descartesresearch/teastore-registry
-docker run --name registry -p 10000:8080 --network teastore -d descartesresearch/teastore-registry
 sleep 2
+PrintSuccess "Setting up Teastore"
+sleep 1
 
-PrintSuccess "Setting up TeaStore DB"
-docker pull descartesresearch/teastore-db
-docker run --name db -p 3306:3306 --network teastore -d descartesresearch/teastore-db
-sleep 2
+#DISCOVERY SERVER
+docker run -P --name sefa-eureka -d --network teastore eureka
 
-PrintSuccess "Setting up TeaStore Persistence"
-docker pull descartesresearch/teastore-persistence
-docker run --name persistence -e "REGISTRY_HOST=registry" -e "HOST_NAME=persistence" -e "DB_HOST=db" -e "DB_PORT=3306" -p 1111:8080 --network teastore -d descartesresearch/teastore-persistence
-sleep 2
+#REGISTRY
+docker run --name registry -p 1000:8080 --network teastore -d teastore-registry
 
-PrintSuccess "Setting up TeaStore Store"
-docker pull descartesresearch/teastore-auth
-docker run --name auth -e "REGISTRY_HOST=registry" -e "HOST_NAME=auth" -p 2222:8080 --network teastore -d descartesresearch/teastore-auth
-sleep 2
+#DB
+docker run --name db -p 3306:3306 --network teastore -d teastore-db
 
-PrintSuccess "Setting up TeaStore Recommender"
-docker pull descartesresearch/teastore-recommender
-docker run --name recommender -e "REGISTRY_HOST=registry" -e "HOST_NAME=recommender" -p 3333:8080 --network teastore -d descartesresearch/teastore-recommender
-sleep 2
+#PERSISTENCE
+docker run --name persistence -e "REGISTRY_HOST=registry" -e "HOST_NAME=persistence" -e "DB_HOST=db" -e "DB_PORT=3306" -p 1111:8080 --network teastore -d teastore-persistence
 
-PrintSuccess "Setting up TeaStore Image Provider"
-docker pull descartesresearch/teastore-image
-docker run --name image -e "REGISTRY_HOST=registry" -e "HOST_NAME=image" -p 4444:8080 --network teastore -d descartesresearch/teastore-image
-sleep 2
+#STORE/AUTH
+docker run --name auth -e "REGISTRY_HOST=registry" -e "HOST_NAME=auth" -p 2222:8080 --network teastore -d teastore-auth
 
-PrintSuccess "Setting up TeaStore WebUI"
-docker pull descartesresearch/teastore-webui
-docker run --name webui -e "REGISTRY_HOST=registry" -e "HOST_NAME=webui" -p 8080:8080 --network teastore -d descartesresearch/teastore-webui
+#RECOMMENDER
+docker run --name recommender -e "REGISTRY_HOST=registry" -e "HOST_NAME=recommender" -p 3333:8080 --network teastore -d teastore-recommender
 
-PrintSuccess "DONE"
+#IMAGE
+docker run --name image -e "REGISTRY_HOST=registry" -e "HOST_NAME=image" -p 4444:8080 --network teastore -d teastore-image
 
+#WEBUI
+docker run --name webui -e "REGISTRY_HOST=registry" -e "HOST_NAME=webui" -p 8080:8080 --network teastore -d teastore-webui
 
